2469037b1efb0f0eaf2c70baa70c1cc15785c65f
Brendan <bvdv586@gmail.com>
2023-04-28 19:44:51 -0400

    added some validation to login page

--- IST440WGroup4/Pages/Index.cshtml.cs
+++ IST440WGroup4/Pages/Index.cshtml.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Identity.Client;
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Security.Claims;
+using static IST440WGroup4.Pages.Dashboard.IndexModel.Clinician;
 
 namespace IST440WGroup4.Pages
 {
@@ -17,14 +21,60 @@ namespace IST440WGroup4.Pages
         [BindProperty]
         public Credential credential { get; set; }
 
+        public List<LoginInfo> listLogins = new List<LoginInfo>();
+
         public void OnGet()
         {
-            
+            try
+            {
+                String connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=GPHDatabase;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM Login";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                LoginInfo loginInfo = new LoginInfo();
+                                loginInfo.loginID = "" + reader.GetInt32(0);
+                                loginInfo.password = reader.GetString(1);
+
+                                listLogins.Add(loginInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            catch { }
 
         }
 
-        
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+                if (credential.UserName == "2063" && credential.Password == "password") 
+                { 
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "2036")
+
+                    };
+                    var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                    ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                    await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                    return RedirectToPage("/Dashboard/Index");
+            }
+
+            return Page();
+        }
     }
+
     public class Credential
     {
         [Required]
@@ -32,5 +82,14 @@ namespace IST440WGroup4.Pages
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
+    }
+
+    public class LoginInfo
+    {
+        [Required]
+        public String loginID;
+        [Required]
+        public String password;
+
     }
 }
---
+++ LoginSQLQuery.sql
@@ -0,0 +1,108 @@
+﻿INSERT INTO Login (loginID, password)
+
+VALUES ( 2063, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 2097, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 2808, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 19067, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 28001, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 30247, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 37028, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 56671, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 62901, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 106088, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 66183, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 270686, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 301208, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 301212, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 301439, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 302434, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 303173, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 303840, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 19778, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 19780, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 19782, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 19784, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 19786, 'password');
+
+INSERT INTO Login (loginID, password)
+
+VALUES ( 19788, 'password');
+
+
+
+
+
+
+
+
+
+
+
+
+
